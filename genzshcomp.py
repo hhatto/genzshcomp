cc9f9e6bf2f4e80b4818c636b5c219650f8454e0
Hideo Hattori <devnull@localhost>
2009-11-19 04:00:15 +0900

    change to method name from parse() to get(). filename completion.

--- genzshcomp.py
+++ genzshcomp.py
@@ -1,6 +1,5 @@
 """automatic generated to zsh completion function file"""
 
-
 __version__ = '0.0.1'
 __author__ = 'Hideo Hattroi <hhatto.jp@gmail.com>'
 __license__ = 'NewBSDLicense'
@@ -20,16 +19,29 @@ class ZshCompletionGenerator(object):
             ret.append(s)
         return "".join(ret)
 
-    def parse(self):
+    def get(self):
         ret = []
         ret.append("#compdef %s\n" % self.commandname)
         ret.append("typeset -A opt_args")
         ret.append("local context state line\n")
         ret.append("_arguments -s -S \\")
         for action in self.parser._actions:
+            directory_comp = "-/"   ## default setting
+            if action.metavar:
+                metavar = action.metavar
+                #if "dir" in metavar.lower():
+                #    directory_comp = "-/"
+            else:
+                metavar = ""
             for opt in action.option_strings:
-                tmp = "  \"%s[%s]\" \\" % (opt,
-                      self._escape_squarebracket(action.help))
+                tmp = "  \"%s[%s]:%s:_files %s\" \\" % (opt,
+                      self._escape_squarebracket(action.help),
+                      metavar, directory_comp)
                 ret.append(tmp)
+        ret.append("  \":files:->files\" \\")
+        state = "case $state in\n"\
+                "(files)\n"\
+                "  _files -/ && return 0\n  ;;\nesac\n"
         ret.append("  && return 0")
+        ret.append(state)
         return "\n".join(ret)
